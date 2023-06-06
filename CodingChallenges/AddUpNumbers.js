b05edb7faf2476b8e8519e3d402f575fac42f223
RebeccaMines <becca.mines22297@gmail.com>
2023-06-05 19:01:01 -0500

    coding challenge 5 and 6

---
+++ CodingChallenges/AddUpNumbers.js
@@ -0,0 +1,14 @@
+function addUp(num) {
+    let sum;
+    for (let i = 1; i <= num; i++) {
+    sum += i;
+}
+
+    return sum;
+
+}
+
+
+
+
+
---
+++ CodingChallenges/FindLowerCase.js
@@ -0,0 +1,10 @@
+function detectWord(str) {
+    let word = "";
+    for (i = 0; i <str.length; i++) {
+        if (str[i].toLowerCase() = str[i]) {
+            letter = str[i];
+            word += letter;
+        } 
+    }
+    return word;
+}
